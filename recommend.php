f97f17452c407dcbcda2a669254e0c4fb12e29c6
Peter Larson <plarson@jiffymedia.com>
2014-03-03 14:57:53 -0700

    in the middle of merging js

--- recommend.php
+++ recommend.php
@@ -27,7 +27,8 @@
 	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script> 
 	
 	<script type="text/javascript" src="js/libs/jquery.fullPage.min.js"></script>
-	<script type="text/javascript" src="js/app.js"></script>
+	<!-- <script type="text/javascript" src="js/app.js"></script> -->
+	<script type="text/javascript" src="js/site.js"></script>
 
 </head>
 <body class="results">
