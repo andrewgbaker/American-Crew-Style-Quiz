b07b8803d789bf2644e7347a316fe98ba493a6fa
Andrew Baker <agb.baker@gmail.com>
2014-03-13 15:11:37 -0600

    tweak quote styles to accomodate length

---
+++ gentleman.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<!--[if lte IE 9 ]>    <html lang="en" class="no-js ie ie8"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html class='no-js' lang='en'>
+  <!--<![endif]-->
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>American Crew Style Select</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="author" content="Karsh Hagan">  	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+	<script type="text/javascript" src="js/libs/modernizr.js"></script>
+	
+	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+  
+	<link rel="stylesheet" href="css/style.css" />
+	
+	<!--[if IE]>
+		<script type="text/javascript">
+			 var console = { log: function() {} };
+		</script>
+	<![endif]-->
+		
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<script>
+	if (!window.jQuery) {
+	    document.write('<script src="/js/jquery-1.8.3.min.js"><\/script>');
+	}
+	</script>
+
+	<script type="text/javascript" src="js/plugins.js"></script>
+	<!-- <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script> -->
+	
+	<script type="text/javascript" src="js/libs/jquery.fullPage.min.js"></script>
+	<script type="text/javascript" src="js/app.js"></script>
+	<script type="text/javascript" src="js/site.js"></script>
+
+</head>
+<body class="results">
+
+<header>
+	<a href="#" class="logo"></a>
+	<article>
+		<h1>You Are a Gentleman</h1>
+		<p>Your drinks are stiff and so is your upper lip. You give women hope that chivalry might not be dead after all. And you're either dressed well or not at all. You need a look that has structure, order and plenty of confidence. <a href="#" class="share">Share +</a> </p>
+	</article>
+	<a href="#" class="logo"></a>
+	<div class="expand"><div></div></div>
+</header>
+
+<div class="logo looks_logo hidden" data-script="LooksLogo"></div>
+
+<div class="section look_down" id="section1">
+	<!-- START Look 1 -->
+	<div class="slide clearfix" data-anchor="one">
+		<article>
+			<div class="look_wrap">
+				<div class="mobile_look_img"><img src="img/gentleman1_mobile.jpg" alt="the single malt" /> </div>
+				<h2>Gentleman - <span>The Looks</span></h2>
+				<nav class="looks_nav clearfix">
+					<a href="#" class="active_look">
+						<div class="progress"><span></span></div>
+					</a>
+					<a href="#">
+						<div class="progress"><span></span></div>
+					</a>
+					<a href="#">
+						<div class="progress"><span></span></div>
+					</a>
+				</nav>
+				<p>If you’re on your way out for a nice dinner, this style is a great choice. Its strong perimeter and ample texture show that your taste is especially refined.</p>
+				<p class="quote">Apply Light Hold Styling gel first, and use Boost Powder to exaggerate your hair's fullness and finish the style. Let the natural texture of your hair dictate the final look.</p>
+				<p>-Paul Wilson, American Crew Artistic Director</p>
+				<div class="product clearfix">
+					<div class="wrap">
+						<img src="img/product.jpg" />
+					</div>
+					<div class="cta">
+						<div class="start_btn secondary"><a href="#">Find a Stylist</a></div>
+						<div class="start_btn"><a href="#">Buy Product</a></div>
+					</div>
+				</div>
+				<p>To create this style, you'll need <a href="#">American Crew Light Hold Styling Gel</a>, <a href="#">Boost Powder</a> and to <a href="img/gentleman_1_guide.jpg" target="_blank">download this image</a> so your stylist can show you the step-by-step instructions.</p>
+				<h6>Not Where you Thought You'd End Up? <a href="#">Restart the Quiz </a></h6>
+			</div>
+		</article>
+		<div class="look_img" style="background: url(img/looks/gentleman1.jpg) no-repeat top center"></div>
+	</div>
+	<!-- END LOOK ONE -->
+    <div class="slide" data-anchor="two"> 
+    	<article>
+			<div class="look_wrap">
+				<div class="mobile_look_img"><img src="img/single_malt_mobile.jpg" alt="the single malt" /> </div>
+				<h2>Gentleman - <span>The Looks</span></h2>
+				<nav class="looks_nav clearfix">
+					<a href="#">
+						<div class="progress"><span></span></div>
+					</a>
+					<a href="#" class="active_look">
+						<div class="progress"><span></span></div>
+					</a>
+					<a href="#">
+						<div class="progress"><span></span></div>
+					</a>
+				</nav>
+				<p>If you want a great look for a leisurely Sunday afternoon, this style has a structure that lets loose without losing class.</p>
+				<p class="quote">To create this style, apply Forming Cream throughout your hair, then brush the product through to create the final shape.</p>
+				<p>-Paul Wilson, American Crew Artistic Director</p>
+				<div class="product clearfix">
+					<div class="wrap">
+						<img src="img/product.jpg" />
+					</div>
+					<div class="cta">
+						<div class="start_btn secondary"><a href="#">Find a Stylist</a></div>
+						<div class="start_btn"><a href="#">Buy Product</a></div>
+					</div>
+				</div>
+				<p>To create this style, you'll need <a href="#">Light Hold Texture Lotion</a>, <a href="#">Forming Cream</a> and to <a href="img/gentleman_1_guide.jpg" target="_blank">download this image</a> so your stylist can show you the step-by-step instructions.</p>
+				<h6>Not Where you Thought You'd End Up? <a href="#">Restart the Quiz </a></h6>
+			</div>
+		</article>
+		<div class="look_img" style="background: url(img/looks/gentleman1.jpg) no-repeat top center"></div>
+    </div>
+    <!-- END LOOK TWO -->
+    <div class="slide" data-anchor="three">
+    	<article>
+			<div class="look_wrap">
+				<div class="mobile_look_img"><img src="img/single_malt_mobile.jpg" alt="the single malt" /> </div>
+				<h2>Gentleman - <span>The Looks</span></h2>
+				<nav class="looks_nav clearfix">
+					<a href="#">
+						<div class="progress"><span></span></div>
+					</a>
+					<a href="#">
+						<div class="progress"><span></span></div>
+					</a>
+					<a href="#" class="active_look">
+						<div class="progress"><span></span></div>
+					</a>
+				</nav>
+				<p>When you’re looking to close a deal, this style—with its strong side part and controlled texture—gets the job done every time.</p>
+				<p class="quote">“To create this look, towel dry your hair, apply Pomade and use a brush to shape your style.</p>
+				<p>- Paul Wilson, American Crew Artistic Director</p>
+				<div class="product clearfix">
+					<div class="wrap">
+						<img src="img/product.jpg" />
+					</div>
+					<div class="cta">
+						<div class="start_btn secondary"><a href="#">Find a Stylist</a></div>
+						<div class="start_btn"><a href="#">Buy Product</a></div>
+					</div>
+				</div>
+				<p>To create this style, you'll need <a href="#">Light Hold Styling Gel</a>, <a href="#">Pomade</a> and to <a href="img/gentleman_1_guide.jpg" target="_blank">download this image</a> so your stylist can show you the step-by-step instructions.</p>
+				<h6>Not Where you Thought You'd End Up? <a href="#">Restart the Quiz </a></h6>
+			</div>
+		</article>
+		<div class="look_img" style="background: url(img/looks/gentleman1.jpg) no-repeat top center"></div>
+    </div>
+    <!-- END LOOK THREE -->
+    
+</div>
+
+</body>
+</html>
